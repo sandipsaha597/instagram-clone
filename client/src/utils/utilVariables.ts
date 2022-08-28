68309590e62eeec98eb25198a2deeef35f7ab338
sandip-saha <>
2022-08-29 00:45:25 +0530

    production backend endpoints added

--- client/src/utils/utilVariables.ts
+++ client/src/utils/utilVariables.ts
@@ -1,3 +1,9 @@
-export const DOMAIN = 'http://localhost:8080/api'
-export const SOCKET_IO_DOMAIN = 'http://localhost:8081'
+export const DOMAIN =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8080/api'
+    : 'http://43.205.140.166:8080/api'
+export const SOCKET_IO_DOMAIN =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8081/api'
+    : 'http://43.205.140.166:8081/api'
 export const border = '1px solid rgb(219, 219, 219)'
