0c5786d74e64f2f32d7a5ce4eff9f54685eed8c7
sandip-saha <>
2022-08-29 20:32:13 +0530

    socket_io_domain fix

--- client/src/utils/utilVariables.ts
+++ client/src/utils/utilVariables.ts
@@ -4,6 +4,6 @@ export const DOMAIN =
     : 'http://43.205.140.166:8080/api'
 export const SOCKET_IO_DOMAIN =
   process.env.NODE_ENV === 'development'
-    ? 'http://localhost:8081/api'
-    : 'http://43.205.140.166:8081/api'
+    ? 'http://localhost:8081'
+    : 'http://43.205.140.166:8081'
 export const border = '1px solid rgb(219, 219, 219)'
