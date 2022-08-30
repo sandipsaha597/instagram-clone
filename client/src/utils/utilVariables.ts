0982b87e8a2aeb65ad583896a3529bd55a500854
sandip-saha <>
2022-08-30 18:41:25 +0530

    endpoint changed because elastic ip added to ec2 instance

--- client/src/utils/utilVariables.ts
+++ client/src/utils/utilVariables.ts
@@ -1,9 +1,9 @@
 export const DOMAIN =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8080/api'
-    : 'http://ec2-43-205-140-166.ap-south-1.compute.amazonaws.com:8080/api'
+    : 'http://ec2-13-233-240-100.ap-south-1.compute.amazonaws.com:8080/api'
 export const SOCKET_IO_DOMAIN =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8081'
-    : 'http://ec2-43-205-140-166.ap-south-1.compute.amazonaws.com:8081'
+    : 'http://ec2-13-233-240-100.ap-south-1.compute.amazonaws.com:8081'
 export const border = '1px solid rgb(219, 219, 219)'
